aab73d1eed1c634196df9c686a12912036149089
juan123xyz <juanestebanramirezhermosa@gmail.com>
2024-11-17 14:54:35 -0500

    Update GestorDeHorario.java

--- Horario_W_GUI/src/main/java/GestorDeHorario.java
+++ Horario_W_GUI/src/main/java/GestorDeHorario.java
@@ -1,3 +1,21 @@
-public class GestorDeHorario {
+ public void agregarHorario(Horario horario) {
+        horarios.add(horario);
+    }
 
+    public void mostrarHorariosPorAsignatura(String nombreAsignatura) {
+        boolean encontrado = false;
+        for (Horario horario : horarios) {
+            if (horario.getAsignatura().getNombre().equalsIgnoreCase(nombreAsignatura)) {
+                System.out.println(horario);
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("No se encontraron horarios para la asignatura " + nombreAsignatura);
+        }
+    }
+
+    public List<Horario> getHorarios() {
+        return horarios;
+    }
 }
