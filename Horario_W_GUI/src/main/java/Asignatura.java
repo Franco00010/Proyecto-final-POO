2d0c25e695fd1fb30c3b9e4df81004777b6101f7
juan123xyz <juanestebanramirezhermosa@gmail.com>
2024-11-17 14:50:48 -0500

    Update Asignatura.java

--- Horario_W_GUI/src/main/java/Asignatura.java
+++ Horario_W_GUI/src/main/java/Asignatura.java
@@ -2,17 +2,18 @@
 // ???
 public class Asignatura {
     private String nombre;
+    private int creditos;
 
-    public Asignatura(String nombre) {
+    public Asignatura(String nombre, int creditos) {
         this.nombre = nombre;
+        this.creditos = creditos;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    @Override
-    public String toString() {
-        return "Asignatura: " + nombre;
+    public int getCreditos() {
+        return creditos;
     }
 }
